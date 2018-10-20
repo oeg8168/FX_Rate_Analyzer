8892ff45d19b5b347f1511e2c5d12c063d6a0584
Owen Ou <oeg8168@gmail.com>
2018-10-20 10:10:38 +0000

    do basic data processing

--- main.R
+++ main.R
@@ -3,3 +3,12 @@ rm(list = ls()) # Clear environment
 
 # Source all functions
 lapply(list.files("./functions/", full.names = TRUE), source)
+# Read FX rate data
+fx_data <- getFXData()
+
+# Convert column names
+colnames(fx_data)[1] <- "Date"
+colnames(fx_data) <- convertCurrencyNames(colnames(fx_data))
+
+# Convert type of "Date" column from character to Date
+fx_data$Date <- as.Date(fx_data$Date)
