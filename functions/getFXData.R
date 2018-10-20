6777424a0bf797b26f5add4bedd8e21d8d2db979
Owen Ou <oeg8168@gmail.com>
2018-10-20 10:02:43 +0000

    add function getFXData() to read data

---
+++ functions/getFXData.R
@@ -0,0 +1,23 @@
+#' Read FX rate data from .csv files
+#'
+#' @return dataframe of FX rate data
+#' @export
+#'
+#' @examples
+#' fx_data <- getFXData()
+getFXData <- function() {
+    filenames = list.files("./FX_Rate_Data/", full.names = TRUE)
+
+    datalist = lapply(filenames,
+                      function(x) {
+                          read.csv(
+                              file = x,
+                              header = TRUE,
+                              stringsAsFactors = FALSE
+                          )
+                      })
+
+    Reduce(function(x, y) {
+        merge(x, y, all = TRUE)
+    }, datalist)
+}
