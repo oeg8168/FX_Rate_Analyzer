54e0bc9c88e7d8a578ffffe41ea0fbb13dd849d4
Owen Ou <oeg8168@gmail.com>
2018-10-22 15:01:52 +0000

    draw line plot (both all columns and one column)

--- main.R
+++ main.R
@@ -3,6 +3,7 @@ rm(list = ls()) # Clear environment
 
 # Source all functions
 lapply(list.files("./functions/", full.names = TRUE), source)
+
 # Read FX rate data
 fx_data <- getFXData()
 
@@ -12,3 +13,9 @@ colnames(fx_data) <- convertCurrencyNames(colnames(fx_data))
 
 # Convert type of "Date" column from character to Date
 fx_data$Date <- as.Date(fx_data$Date)
+
+# Get line plot using ggplot2
+p1 <- getLinePlot(fx_data)
+show(p1)
+p2 <- getLinePlot(fx_data[, c("Date", "USD.TWD")])
+show(p2)
